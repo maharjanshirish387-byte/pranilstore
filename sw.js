ad2637796acb3ce53668087a9544f2563ec9f661
ShirishMaharjanX <maharjanshirish36@gmail.com>
2026-02-19 13:50:14 +0545

    Create sw.js service worker that clears old caches and prevents stale content

---
+++ sw.js
@@ -0,0 +1,21 @@
+// sw.js - Service Worker
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
